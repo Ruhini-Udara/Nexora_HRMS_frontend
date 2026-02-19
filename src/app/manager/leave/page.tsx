eb533722e24bb2623288933fe186b397ad0710d0
Ruhini <ruhiniudara2@gmail.com>
2026-02-19 15:39:18 +0530

    Manager Leave Requests Page Created

---
+++ src/app/manager/leave/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from 'react';
+import LeaveStats from '@/components/manager/leave/LeaveStats';
+import LeaveRequestsTable from '@/components/manager/leave/LeaveRequestsTable';
+
+const LeaveRequestsPage = () => {
+    return (
+        <div className="p-8 space-y-6">
+            <h2 className="text-xl font-bold text-gray-800 dark:text-white mb-6">Leave Requests</h2>
+
+            {/* Stats Overview */}
+            <LeaveStats />
+
+            {/* Table Card */}
+            <LeaveRequestsTable />
+        </div>
+    );
+};
+
+export default LeaveRequestsPage;
